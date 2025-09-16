a35eef56a900c3ac7beb3d1c5498df12bc3d2871
Andly <andlybras.desenvolvedor@outlook.com>
2025-09-16 16:30:06 -0400

    Prepara para a produção

--- configuracoes_projeto/settings.py
+++ configuracoes_projeto/settings.py
@@ -1,12 +1,17 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-25+*s#^81&id&%)m-v+*n_^9*+8$w6)5uv4&8kj20km=#ki@yt'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roraimatradehub.ddns.net', '18.228.117.135']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -28,6 +33,7 @@ INSTALLED_APPS = [
     'taggit',
     'tinymce',
     'django_recaptcha',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -62,10 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'configuracoes_projeto.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -117,8 +120,47 @@ TAGGIT_STRIP_UNICODE_WHEN_SLUGIFYING = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 LOGIN_REDIRECT_URL = '/quero-vender/dashboard/' 
 
 LOGIN_URL = '/quero-vender/acessar/'
+
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+    
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
+    AWS_DEFAULT_ACL = 'public-read'
+    
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    
+RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
+
+RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
